37ed09b0ae1dc11c2523638abbb8144256cf8833
mkinty <kintymoustapha@gmail.com>
2021-01-08 15:40:18 +0100

    first commit

--- todoapp/wsgi.py
+++ todoapp/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import DjangoWhiteNoise
+# from django.core.wsgi import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoapp.settings')
 
 application = get_wsgi_application()
 # add DjangoWhiteNoise for dev
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
